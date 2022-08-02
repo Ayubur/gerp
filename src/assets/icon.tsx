faf77822e56f24182a828782dcd1c2b4adb2baf7
Abdur Razzak Uzzal <abdurrazzakuzzal.aru@gmail.com>
2022-08-02 10:36:33 +0600

    table

---
+++ src/assets/icon.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export const RightArrowIcon: React.FC = () => (
+    <svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9 9L9.70711 8.29289L10.4142 9L9.70711 9.70711L9 9ZM1.70711 0.292893L9.70711 8.29289L8.29289 9.70711L0.292893 1.70711L1.70711 0.292893ZM9.70711 9.70711L1.70711 17.7071L0.292893 16.2929L8.29289 8.29289L9.70711 9.70711Z" fill="black" />
+    </svg>
+)
+
+
+export const AddRoundFillIcon: React.FC = () => (
+    <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M19 9C19 13.9706 14.9706 18 10 18C5.02944 18 1 13.9706 1 9C1 4.02944 5.02944 0 10 0C14.9706 0 19 4.02944 19 9ZM10 15C9.44771 15 9 14.5523 9 14V10H5C4.44772 10 4 9.55229 4 9C4 8.44771 4.44772 8 5 8H9V4C9 3.44772 9.44771 3 10 3C10.5523 3 11 3.44772 11 4V8H15C15.5523 8 16 8.44771 16 9C16 9.55229 15.5523 10 15 10H11V14C11 14.5523 10.5523 15 10 15Z" fill="#606060" />
+    </svg>
+)
--- src/pages/contacts/contactLists.tsx
+++ src/pages/contacts/contactLists.tsx
@@ -1,7 +1,276 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Table, Dropdown, Menu, Space, Row, Col, Divider, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import "./contactList.css";
+import { AddRoundFillIcon, RightArrowIcon } from "../../assets/icon";
 
-const ContactLists = ()=> (
-    <div>Contact Lists</div>
-)
+interface DataType {
+    key: React.Key;
+    business_name: string;
+    job_title: string;
+    first_name: string;
+    last_name: string;
+    address: String;
+    city: string;
+    mobile_number: string;
+    email: string;
+    contact_type: string;
+    location_group: string;
+    status: string;
+}
+
+const menu = (
+    <Menu
+        items={[
+            {
+                key: '1',
+                label: (
+                    <a>
+                        Details
+                    </a>
+                ),
+            },
+            {
+                key: '2',
+                label: (
+                    <a >
+                        Edit
+                    </a>
+                ),
+            },
+            {
+                key: '3',
+                label: (
+                    <a>
+                        Delete
+                    </a>
+                ),
+            }
+        ]}
+    />
+);
+
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'Business Name',
+        dataIndex: 'business_name',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.business_name.toUpperCase() < b.business_name.toUpperCase() ? -1 : 1,
+    },
+    {
+        title: 'Job Title',
+        dataIndex: 'job_title',
+        sorter: (a, b) => a.job_title.toUpperCase() < b.job_title.toUpperCase() ? -1 : 1,
+    },
+    {
+        title: 'First Name',
+        dataIndex: 'first_name',
+        sorter: (a, b) => a.first_name.toUpperCase() < b.first_name.toUpperCase() ? -1 : 1,
+        render(value, record, index) {
+            return (
+                <b>{value}</b>
+            )
+        },
+    },
+    {
+        title: 'Last Name',
+        dataIndex: 'last_name',
+        sorter: (a, b) => a.last_name.toUpperCase() < b.last_name.toUpperCase() ? -1 : 1,
+        render(value, record, index) {
+            return (
+                <b>{value}</b>
+            )
+        },
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+    },
+    {
+        title: 'City',
+        dataIndex: 'city',
+        sorter: (a, b) => a.city.toUpperCase() < b.city.toUpperCase() ? -1 : 1,
+        filters: [
+            {
+                text: 'Gulshan',
+                value: 'Gulshan',
+            },
+            {
+                text: 'Mirpur',
+                value: 'Mirpur',
+            },
+        ],
+    },
+    {
+        title: 'Mobile No.',
+        dataIndex: 'mobile_number',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+    },
+    {
+        title: 'Contact Type',
+        dataIndex: 'contact_type',
+        sorter: (a, b) => a.contact_type.toUpperCase() < b.contact_type.toUpperCase() ? -1 : 1,
+    },
+    {
+        title: 'Location Group',
+        dataIndex: 'location_group',
+        sorter: (a, b) => a.location_group.toUpperCase() < b.location_group.toUpperCase() ? -1 : 1,
+    },
+    {
+        title: '',
+        dataIndex: 'status',
+        render(value, record, index) {
+            return (
+                <div style={{ height: "10px", width: "10px", background: "#00C489", borderRadius: "50%" }}>
+
+                </div>
+            )
+        },
+    },
+    {
+
+        dataIndex: 'action',
+        render(value, record, index) {
+            return (
+                <Dropdown overlay={menu}>
+                    <a>...</a>
+                </Dropdown>
+            )
+        },
+    },
+];
+
+
+const data: DataType[] = [];
+for (let i = 0; i < 46; i++) {
+    data.push({
+        key: i,
+        business_name: `Gononet LLC`,
+        job_title: `Store Manager`,
+        first_name: `Mohammad`,
+        last_name: `Hossain`,
+        address: `London, Park Lane no. ${i}`,
+        city: `Gulshan ${i}`,
+        mobile_number: `01755590478`,
+        email: `nevaeh@example.com`,
+        contact_type: `Consignment`,
+        location_group: `GROUP 0${i}`,
+        status: ``,
+    });
+}
+
+
+const ContactLists: React.FC = () => {
+
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+    const [isMobile, setisMobile] = useState(false);
+
+    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys);
+        setSelectedRowKeys(newSelectedRowKeys);
+    };
+
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+
+
+    React.useLayoutEffect(() => {
+        function updateSize() {
+
+            if (window.innerWidth < 900) {
+                setisMobile(true);
+            } else {
+                setisMobile(false);
+            }
+        }
+        window.addEventListener("resize", updateSize);
+        updateSize();
+        return () => window.removeEventListener("resize", updateSize);
+    }, []);
+
+    return (
+        <div>
+            <div style={{ marginBottom: 16 }}>
+                <span style={{ marginLeft: 8 }}>
+                    {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+                </span>
+            </div>
+
+            {isMobile ? (
+                <div className="card">
+                    <Row align="middle" justify="space-between">
+                        <Col>
+                            <b>Store Manager</b>
+                        </Col>
+                        <Col>
+                            Gulshan
+                        </Col>
+                    </Row>
+                    <Divider style={{ margin: "10px 0" }} />
+                    <Space direction="vertical" style={{ width: "100%" }}>
+                        <Row align="middle" justify="center" gutter={20}>
+                            <Col>
+                                <AddRoundFillIcon />
+                            </Col>
+                            <Col span={20}>
+                                <Space direction="horizontal" align="start">
+                                    <div>
+                                        <b>Mohammad Hossain</b>
+                                        <div>01755590478</div>
+                                    </div>
+                                    <div style={{ height: "10px", width: "10px", background: "#00C489", borderRadius: "50%" }}></div>
+                                </Space>
+                            </Col>
+                        </Row>
+
+                        <div className="cardExpanded">
+                            <Space direction="vertical">
+
+                                <div>
+                                    <b>Address: </b>
+                                    Arhitektu iela 25 - 55, Daugavpils, LV-5410
+                                </div>
+
+                                <div>
+                                    <b>Email: </b>
+                                    nevaeh@example.com
+                                </div>
+
+                                <div>
+                                    <b>Contact Type: </b>
+                                    Consignment
+                                </div>
+
+                                <div>
+                                    <b>Location Group: </b>
+                                    GROUP 001
+                                </div>
+
+                            </Space>
+                        </div>
+                    </Space>
+                </div>
+            ) : (
+                <Table
+                    rowSelection={rowSelection}
+                    columns={columns}
+                    dataSource={data}
+                    scroll={{ y: "55vh" }}
+                    pagination={{ pageSize: 50 }}
+                />
+            )}
+
+
+        </div>
+    );
+
+}
+
 
 export default ContactLists;
